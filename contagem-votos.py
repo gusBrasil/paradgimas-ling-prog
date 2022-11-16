391d3765eb545be0635ce5fda0bb6f71a465cd55
gusBrasil <gustavomedbra@gmail.com>
2022-11-16 19:06:29 -0300

    update

--- contagem-votos.py
+++ contagem-votos.py
@@ -1,3 +1,8 @@
+# Programa elaborado por Gustavo Medeiros Brasil para a matéria de Paradigmas de Linguagens de Programação
+# O seu objetivo é simular um ambiente de votações, com o programa recebendo e processando os dados de cada "cédula",
+# em seguida emitindo um relatório com os resultados.
+# 
+
 a = 0
 j = 0
 m = 0
@@ -19,42 +24,47 @@ def calculaVotos(a, j, m, b, n):
     print('O número de eleitores que compareceram às urnas foi de: ' + str(TotalVotos))
 
 
+def calculaEmpate(a, j, m):
+    if a == m or a == j or m == j:
+        print('Votação em Segundo Turno!')
+
+
 while True:
     voto = input(
         'Vote em seu candidato. Antônio(1), José(2), Maria(3), Branco(4), nulo(*) ')
+    confirmacao = input('confirma? (S/N) ')
 
     if voto in ('1', '2', '3', '4', '*'):
-        confirmacao = input('confirma? (S/N) ')
 
         if voto == '1':
             if confirmacao == 'S':
                 a = a + 1
                 print('Confirmado!')
             elif confirmacao == 'N':
-                break
+                continue
 
         elif voto == '2':
             if confirmacao == 'S':
                 j = j + 1
                 print('Confirmado')
             elif confirmacao == 'N':
-                break
+                continue
 
         elif voto == '3':
             if confirmacao == 'S':
                 m = m + 1
                 print('Confirmado')
             elif confirmacao == 'N':
-                break
+                continue
 
         elif voto == '4':
             if confirmacao == 'S':
                 b = b + 1
                 print('Confirmado')
             elif confirmacao == 'N':
-                break
+                continue
 
-        elif voto == '*':
+        else:
             if confirmacao == 'S':
                 n = n + 1
                 print('Confirmado')
@@ -66,4 +76,6 @@ while True:
         print(j)
         print(m)
         calculaVenc(a, j, m)
+        calculaEmpate(a, j, m)
         calculaVotos(a, j, m, b, n)
+        break
