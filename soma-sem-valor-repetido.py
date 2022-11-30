b28d5d219770ae0d824b491fcdc13f629b9561d9
Gustavo Brasil <64227804+gusBrasil@users.noreply.github.com>
2022-11-30 09:17:52 -0300

    Update soma-sem-valor-repetido.py

--- soma-sem-valor-repetido.py
+++ soma-sem-valor-repetido.py
@@ -1,6 +1,13 @@
-num1 = int( input("Digite primeiro número: ") )
-num2 = int( input("Digite segundo número: ") )
-num3 = int( input("Digite terceiro número: ") )
+# Programa elaborado por Gustavo Medeiros Brasil para a matéria de Paradigmas de Linguagens de Programação
+# Seu objetivo é receber o valor de três números do usuário e somá-los, caso sejam números diferentes entre si.Se houverem números repetidos,
+# serão desconsiderados pela conta.
+# Disponível também em: https://github.com/gusBrasil/paradgimas-ling-prog
+
+
+
+num1 = int( input("Digite o primeiro número: ") )
+num2 = int( input("Digite o segundo número: ") )
+num3 = int( input("Digite o terceiro número: ") )
 
 soma = 0
 
